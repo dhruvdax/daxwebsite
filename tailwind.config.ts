9c5970828fe8897b71b5f08431ab6b2b39077dd3
dhruv daxsws <dhruvdaxsws@gmail.com>
2025-12-16 12:09:18 +0000

    content structure is not coming correctly, fix it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -85,5 +86,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 } satisfies Config;
